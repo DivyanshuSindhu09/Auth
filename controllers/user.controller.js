c1b58951d7153fbdd3438225f78c8143a61b8b94
DivyanshuSindhu09 <divyanshu.sindhu@gmail.com>
2025-11-25 12:08:39 +0530

    logout

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -111,3 +111,23 @@ export const login = async (req, res) => {
         })
     }
 }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly : true,
+            secure : process.env.NODE_ENV === 'production',
+            sameSite : process.env.NODE_ENV === 'development' ? "strict" : "none",
+        })
+
+        return res.json({
+            success : true,
+            message : "Logged out"
+        })
+    } catch (error) {
+        return res.json({
+            success : false,
+            message : error.message
+        })
+    }
+}
